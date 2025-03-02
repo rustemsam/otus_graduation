84d13ffeb98b07a5c41f354dd46f1c4117de78c9
rustem.samigullin <rustem.samigullin@leaseplan.com>
2025-03-02 15:13:27 +0100

    change dockerfile

--- conftest.py
+++ conftest.py
@@ -112,11 +112,11 @@ def browser(request):
         if browser_name == "chrome":
             options = ChromeOptions()
             unique_dir = tempfile.mkdtemp()
-            options.add_argument(f"--user-data-dir={unique_dir}")
-            options.add_argument("--no-sandbox")
-            options.add_argument("--headless")
-            options.add_argument("--disable-dev-shm-usage")
-            options.add_argument("--disable-gpu")
+            #options.add_argument(f"--user-data-dir={unique_dir}")
+            #options.add_argument("--no-sandbox")
+            #options.add_argument("--headless")
+            #options.add_argument("--disable-dev-shm-usage")
+            #options.add_argument("--disable-gpu")
             driver = webdriver.Chrome(options=options)
 
         elif browser_name == "firefox":

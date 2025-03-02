3b6d1574ee733a6195cff898af4748019b72258e
rustem.samigullin <rustem.samigullin@leaseplan.com>
2025-03-02 15:55:11 +0100

    change dockerfile

--- conftest.py
+++ conftest.py
@@ -111,10 +111,8 @@ def browser(request):
     else:
         if browser_name == "chrome":
             options = ChromeOptions()
-
-
             options.add_argument("--no-sandbox")
-            options.add_argument("--headless")
+            #options.add_argument("--headless")
             options.add_argument("--disable-dev-shm-usage")
             options.add_argument("--disable-gpu")
             driver = webdriver.Chrome(options=options)
